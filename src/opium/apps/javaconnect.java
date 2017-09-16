fedec23ba4f8ed4a5cc9e233b64d82c475f24c11
sunny <sunny@sunny-Inspiron-N5050>
2017-09-16 13:43:28 +0530

    new Commit

---
+++ src/opium/apps/javaconnect.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opium.apps;
+
+/**
+ *
+ * @author sunny
+ */
+import java.sql.*;
+import javax.swing.*;
+
+public class javaconnect {
+    Connection conn=null;
+    public static Connection ConnectDB(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc:sqlite:opium.sqlite");
+            //JOptionPane.showMessageDialog(null, "Connected to Opium-Pharmacare System database.");
+            return conn;
+        }catch(ClassNotFoundException | SQLException e){
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+}
